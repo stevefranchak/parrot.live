b794b81078481e316c11e79045e7ccb465f4b35f
Steve Franchak <stevefranchak@gmail.com>
2018-03-30 13:15:19 -0400

    Implemented proof-of-concept for allowing supporting different animation framesets under frames/.

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ const fs = require('mz/fs');
 const http = require('http');
 const {Readable} = require('stream');
 const colors = require('colors/safe');
+const url = require('url');
+const path = require('path');
+const config = require('./config');
 
-const frames = [];
-
-// Setup frames in memory
-fs.readdir('./frames').then(data => { 
-  data.forEach(async frame => {
-    const f = await fs.readFile(`./frames/${frame}`);
-    frames.push(f.toString());
-  })
-});
+const FRAMES_ROOT_DIRECTORY = './frames';
+const cachedFrames = {};
 
 const colorsOptions = ['red', 'yellow', 'green', 'blue', 'magenta', 'cyan', 'white'];
 const numColors = colorsOptions.length;
 
-const streamer = stream => {
+const streamer = (stream, frames) => {
   let index = 0;
   let lastColor = -1;
   let newColor = 0;
@@ -38,15 +34,93 @@ const streamer = stream => {
   }, 70);
 }
 
-const server = http.createServer((req, res) => {
+const getAvailablePaths = async () => {
+  let availablePaths = await fs.readdir(FRAMES_ROOT_DIRECTORY) || [];
+
+  const statObjects = await Promise.all(availablePaths.map((file) => {
+    return fs.lstat(path.join(FRAMES_ROOT_DIRECTORY, file));
+  }));
+
+  availablePaths = availablePaths.filter((file, index) => {
+    return statObjects[index].isDirectory();
+  });
+  return availablePaths;
+};
+
+const getResourceFromRequest = (req) => {
+  let resource = '';
+  const pathname = url.parse(req.url).pathname;
+  const matchedResource = /^\/([^\/]*)/.exec(pathname);
+
+  if (matchedResource instanceof Array && matchedResource.length > 1) {
+    resource = matchedResource[1];
+  }
+
+  return resource;
+};
+
+const getFrames = async (frameSet) => {
+  let frames = [];
+
+  const frameSetRoot = path.join(FRAMES_ROOT_DIRECTORY, frameSet);
+  let frameSetFiles = await fs.readdir(frameSetRoot);
+
+  frameSetFiles.sort((a, b) => {
+    a = parseInt(a);
+    b = parseInt(b);
+    return (a < b) ? -1 : ((a > b) ? 1 : 0);
+  });
+
+  const statObjects = await Promise.all(frameSetFiles.map((file) => {
+    return fs.lstat(path.join(frameSetRoot, file));
+  }));
+
+  frameSetFiles = frameSetFiles.filter((file, index) => {
+    return statObjects[index].isFile() && !isNaN(parseInt(file));
+  });
+
+  for (let file of frameSetFiles) {
+    const f = await fs.readFile(path.join(frameSetRoot, file));
+    frames.push(f.toString());
+  }
+
+  return frames;
+};
+
+const server = http.createServer(async (req, res) => {
   if (req.headers && req.headers['user-agent'] && !req.headers['user-agent'].includes('curl')) {
     res.writeHead(302, {'Location': 'https://github.com/hugomd/parrot.live'});
     return res.end();
   }
+
+  let resource = getResourceFromRequest(req);
+  const availableFrameSets = await getAvailablePaths();
+  if (resource === 'list') {
+     return res.end(JSON.stringify(availableFrameSets, null, 4));
+  }
+
+  if (resource === '') {
+    resource = config.defaultFrameSet;
+  }
+
+  if (availableFrameSets.indexOf(resource) === -1) {
+    res.writeHead(404);
+    return res.end('Not found');
+  }
+
+  if (!cachedFrames.hasOwnProperty(resource)) {
+    cachedFrames[resource] = await getFrames(resource);
+  }
+
+  const frames = cachedFrames[resource];
+  if (!frames.length) {
+    res.end(`No frames found for frameset ${resource}`);
+  }
+
   const stream = new Readable();
   stream._read = function noop () {};
   stream.pipe(res);
-  const interval = streamer(stream);
+  const interval = streamer(stream, frames);
 
   req.on('close', () => {
     stream.destroy();
